ce2629aeb37828672e22e434b5bf4cfba474cc41
Bruno <brunokazuhiro@gmail.com>
2026-01-07 16:04:41 -0300

    Receive data and store on memory

--- conceitos_nest/src/recados/recados.controller.ts
+++ conceitos_nest/src/recados/recados.controller.ts
@@ -29,33 +29,28 @@ export class RecadosController {
   @HttpCode(HttpStatus.OK)
   @Get()
   findAll(@Query() pagination: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars
     const { limit = 10, offset = 0 } = pagination;
     // return `Retorna todos os recados. Limit=${limit}, Offset=${offset}.`;
-    return this.recadosService.hello();
+    return this.recadosService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Essa rota retorna o recado ID ${id}`;
+    return this.recadosService.findOne(id);
   }
 
   @Post()
   create(@Body() body: any) {
-    return body;
+    return this.recadosService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return {
-      id,
-      ...body,
-    };
+    this.recadosService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Essa rota APAGA o recado ID ${id}`;
+    this.recadosService.remove(id);
   }
 }
